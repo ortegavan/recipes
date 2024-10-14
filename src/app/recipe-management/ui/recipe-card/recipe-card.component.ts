b27db13d362078792f013a66601d4d47265b7b94
Van Ortega <ortegavan@live.com>
2024-10-13 23:03:48 -0300

    feat: implementada diretiva para novas receitas

--- src/app/recipe-management/ui/recipe-card/recipe-card.component.ts
+++ src/app/recipe-management/ui/recipe-card/recipe-card.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Recipe } from '../../data/recipe.model';
 import { Category } from '../../data/category.model';
 import { CategoryNamePipe } from '../../util/category-name.pipe';
+import { NewRecipeBadgeDirective } from '../../util/new-recipe-badge.directive'; // Importando a diretiva
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [CategoryNamePipe],
+  imports: [CategoryNamePipe, NewRecipeBadgeDirective], // Importando a diretiva
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
