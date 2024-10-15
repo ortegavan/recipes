becff274a29e65869dfce807a4a341477c2e397e
Van Ortega <ortegavan@live.com>
2024-10-15 20:29:08 -0300

    test: atualização de tipagem na model

--- src/app/recipe-management/ui/recipe-card/recipe-card.component.spec.ts
+++ src/app/recipe-management/ui/recipe-card/recipe-card.component.spec.ts
@@ -14,14 +14,14 @@ describe('RecipeCardComponent', () => {
     fixture = TestBed.createComponent(RecipeCardComponent);
     component = fixture.componentInstance;
     component.categories = [
-      { id: 1, name: 'Sobremesa', icon: 'icon-dessert' },
-      { id: 2, name: 'Prato Principal', icon: 'icon-main' },
+      { id: '1', name: 'Sobremesa', icon: 'icon-dessert' },
+      { id: '2', name: 'Prato Principal', icon: 'icon-main' },
     ];
     component.recipe = {
-      id: 1,
+      id: '1',
       name: 'Exemplo de Receita',
       description: 'Uma deliciosa receita de exemplo.',
-      categoryIds: [1],
+      categoryIds: ['1'],
       imagePath: '/assets/exemplo.jpg',
       ingredients: ['Ingrediente 1', 'Ingrediente 2'],
       instructions: ['Passo 1', 'Passo 2'],
