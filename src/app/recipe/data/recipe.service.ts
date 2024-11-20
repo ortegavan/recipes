f4ab4422d04ede45398e4a8cfc1a05ec224a93fc
Van Ortega <ortegavan@live.com>
2024-11-20 20:17:42 -0300

    refactor: cria o tipo para representar receita

--- src/app/recipe/data/recipe.service.ts
+++ src/app/recipe/data/recipe.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Recipe } from './recipe.model';
 
 @Injectable({
     providedIn: 'root',
@@ -9,7 +11,9 @@ export class RecipeService {
     httpClient = inject(HttpClient);
     apiBaseUrl = environment.apiBaseUrl;
 
-    public get() {
-        return this.httpClient.get(`${this.apiBaseUrl}/api/v1/recipes`);
+    public get(): Observable<Recipe[]> {
+        return this.httpClient.get<Recipe[]>(
+            `${this.apiBaseUrl}/api/v1/recipes`,
+        );
     }
 }
