e9aa5689c31473f265b5b97e374dfee1c6585419
Van Ortega <ortegavan@live.com>
2024-11-21 15:43:42 -0300

    feat: cria diretiva para o badge de nova receita

--- src/app/recipe/ui/recipe-card/recipe-card.component.ts
+++ src/app/recipe/ui/recipe-card/recipe-card.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Category } from '../../data/category.model';
 import { Recipe } from '../../data/recipe.model';
 import { CategoryNamePipe } from '../../util/category-name.pipe';
+import { NewRecipeBadgeDirective } from '../../util/new-recipe-badge.directive';
 
 @Component({
     selector: 'app-recipe-card',
-    imports: [CategoryNamePipe],
+    imports: [CategoryNamePipe, NewRecipeBadgeDirective],
     templateUrl: './recipe-card.component.html',
     styleUrl: './recipe-card.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
