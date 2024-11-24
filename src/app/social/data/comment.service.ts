71d37234db65e21384bae99ecc79ead4c27cf8d9
Van Ortega <ortegavan@live.com>
2024-11-24 18:08:04 -0300

    refactor: migra estado de rxjs para signal

--- src/app/social/data/comment.service.ts
+++ src/app/social/data/comment.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Comment } from './comment.model';
 
@@ -11,8 +11,7 @@ export class CommentService {
     httpClient = inject(HttpClient);
     apiBaseUrl = environment.apiBaseUrl;
 
-    private commentsSubject = new BehaviorSubject<Comment[]>([]);
-    comments$ = this.commentsSubject.asObservable();
+    comments = signal<Comment[]>([]);
 
     public get(recipeId: string): Observable<Comment[]> {
         return this.httpClient
@@ -21,7 +20,13 @@ export class CommentService {
                     recipeId,
                 },
             })
-            .pipe(tap((comments) => this.commentsSubject.next(comments)));
+            .pipe(
+                catchError(() => {
+                    this.comments.set([]);
+                    return of([]);
+                }),
+                tap((comments) => this.comments.set(comments)),
+            );
     }
 
     public add(comment: Comment): Observable<Comment> {
@@ -29,8 +34,8 @@ export class CommentService {
             .post<Comment>(`${this.apiBaseUrl}/api/v1/comments`, comment)
             .pipe(
                 tap((newComment) => {
-                    const currentComments = this.commentsSubject.value;
-                    this.commentsSubject.next([...currentComments, newComment]);
+                    const currentComments = this.comments();
+                    this.comments.set([...currentComments, newComment]);
                 }),
             );
     }
