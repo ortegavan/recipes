f92035b4ca1c457a7f317e4757a5ae964e4d8371
Van Ortega <ortegavan@live.com>
2024-12-21 19:25:52 -0300

    feat: cria serviço que busca receitas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,5 +19,6 @@ export const appConfig: ApplicationConfig = {
                 },
             },
         }),
+        provideHttpClient(),
     ],
 };
